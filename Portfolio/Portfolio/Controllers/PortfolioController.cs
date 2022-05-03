0808ce3f238166f05cecbb671b46737c4a55090c
Your Name <you@example.com>
2022-05-03 12:58:30 -0500

    Initial commit

---
+++ Portfolio/Portfolio/Controllers/PortfolioController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public partial class PortfolioController : Controller
+    {
+        public virtual ActionResult Home()
+        {
+            return View(MVC.Portfolio.Views.Index);
+        }
+    }
+}
